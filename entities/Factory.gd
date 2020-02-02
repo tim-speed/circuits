4fdb15afb835c164363eb49450d218df6e1ab386
Tim Speed <iceczd@gmail.com>
2020-02-02 13:02:46 -0400

    Sprites for factory and broken robot and collision grid map for factory

--- entities/Factory.gd
+++ entities/Factory.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
-var type = 3
+var type_map = [
+	[1, 1, 1, 1],
+	[1, 1, 1, 1],
+	[1, 3, 1, 1]
+]
 var item_name = "Factory"
 
 func _ready():
--- entities/Level.gd
+++ entities/Level.gd
@@ -1,6 +1,7 @@
 extends TileMap
 class_name Level
 
+const CELL_PIXELS = 32
 const MIN_CELL_X = 0
 const MIN_CELL_Y = 0
 const MAX_CELL_X = 21
@@ -22,16 +23,27 @@ var Robot = preload("res://entities/Robot.tscn")
 var factory
 
 func _init():
-	self.cell_quadrant_size = 32
-	self.cell_size.x = 32
-	self.cell_size.y = 32
+	self.cell_quadrant_size = CELL_PIXELS
+	self.cell_size.x = CELL_PIXELS
+	self.cell_size.y = CELL_PIXELS
 
 func _ready():
 	factory = self.get_node("Factory")
 	for child in get_children():
-		set_cellv(world_to_map(child.position), child.type)
-		if child.type == OBJECT and child.item_name == "BrokenFriend":
-			robots_in_need += 1
+		if child.get("type_map") != null:
+			# Type Map for objects larger than 1 cell.. move to its own func?
+			var origin = world_to_map(child.position)
+			var y = 0
+			for row in child.type_map:
+				var x = 0
+				for cell in row:
+					set_cellv(Vector2(origin.x + x, origin.y + y), cell)
+					x += 1
+				y += 1
+		else:
+			set_cellv(world_to_map(child.position), child.type)
+			if child.type == OBJECT and child.item_name == "BrokenFriend":
+				robots_in_need += 1
 	emit_signal("robot_num_change", num_robots, robots_in_need)
 		
 func request_deploy_robot(program):
@@ -41,7 +53,8 @@ func request_deploy_robot(program):
 	var robot = Robot.instance()
 	robot.program = program
 	
-	robot.position = factory.position
+	robot.position = Vector2(factory.position.x + CELL_PIXELS, \
+		factory.position.y + (2 * CELL_PIXELS))
 	
 	self.add_child(robot)
 	
@@ -118,6 +131,7 @@ func go_home(pawn):
 
 func update_pawn_postion(pawn, cell_start, cell_target):
 	set_cellv(cell_target, pawn.type)
-	if get_cellv(cell_start) != FACTORY:
+	var cell_type = get_cellv(cell_start)
+	if cell_type != FACTORY and cell_type != OBSTACLE:
 		set_cellv(cell_start, EMPTY)
 	return map_to_world(cell_target)
